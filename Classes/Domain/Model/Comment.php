f1cdcaa359cb7132351b1568f63d1dc0e57ff46b
Marco Huber <mail@marco-huber.de>
2012-02-25 11:04:47 +0100

    Create the domain model Comment
    ./flow3 kickstart:model MFUG.Showroom Comment
    	commentText:string
    	creationDate:'\Doctrine\Common\DateTime\DateTime'
    	author:'\TYPO3\Party\Domain\Model\Person'
    	published:boolean

---
+++ Classes/Domain/Model/Comment.php
@@ -0,0 +1,146 @@
+<?php
+namespace MFUG\Showroom\Domain\Model;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "MFUG.Showroom".              *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * A Comment
+ *
+ * @FLOW3\Scope("prototype")
+ * @FLOW3\Entity
+ */
+class Comment {
+
+	/**
+	 * The comment text
+	 * @var string
+	 */
+	protected $commentText;
+
+	/**
+	 * The creation date
+	 * @var \Doctrine\Common\DateTime\DateTime
+	 */
+	protected $creationDate;
+
+	/**
+	 * The author
+	 * @var \TYPO3\Party\Domain\Model\Person
+	 */
+	protected $author;
+
+	/**
+	 * The published
+	 * @var boolean
+	 */
+	protected $published;
+
+	/**
+	 * The project
+	 * @var \MFUG\Showroom\Domain\Model\Project
+	 */
+	protected $project;
+
+
+	/**
+	 * Get the Comment's comment text
+	 *
+	 * @return string The Comment's comment text
+	 */
+	public function getCommentText() {
+		return $this->commentText;
+	}
+
+	/**
+	 * Sets this Comment's comment text
+	 *
+	 * @param string $commentText The Comment's comment text
+	 * @return void
+	 */
+	public function setCommentText($commentText) {
+		$this->commentText = $commentText;
+	}
+
+	/**
+	 * Get the Comment's creation date
+	 *
+	 * @return \Doctrine\Common\DateTime\DateTime The Comment's creation date
+	 */
+	public function getCreationDate() {
+		return $this->creationDate;
+	}
+
+	/**
+	 * Sets this Comment's creation date
+	 *
+	 * @param \Doctrine\Common\DateTime\DateTime $creationDate The Comment's creation date
+	 * @return void
+	 */
+	public function setCreationDate(\Doctrine\Common\DateTime\DateTime $creationDate) {
+		$this->creationDate = $creationDate;
+	}
+
+	/**
+	 * Get the Comment's author
+	 *
+	 * @return \TYPO3\Party\Domain\Model\Person The Comment's author
+	 */
+	public function getAuthor() {
+		return $this->author;
+	}
+
+	/**
+	 * Sets this Comment's author
+	 *
+	 * @param \TYPO3\Party\Domain\Model\Person $author The Comment's author
+	 * @return void
+	 */
+	public function setAuthor(\TYPO3\Party\Domain\Model\Person $author) {
+		$this->author = $author;
+	}
+
+	/**
+	 * Get the Comment's published
+	 *
+	 * @return boolean The Comment's published
+	 */
+	public function getPublished() {
+		return $this->published;
+	}
+
+	/**
+	 * Sets this Comment's published
+	 *
+	 * @param boolean $published The Comment's published
+	 * @return void
+	 */
+	public function setPublished($published) {
+		$this->published = $published;
+	}
+
+	/**
+	 * Get the Comment's project
+	 *
+	 * @return \MFUG\Showroom\Domain\Model\Project The Comment's project
+	 */
+	public function getProject() {
+		return $this->project;
+	}
+
+	/**
+	 * Sets this Comment's project
+	 *
+	 * @param \MFUG\Showroom\Domain\Model\Project $project The Comment's project
+	 * @return void
+	 */
+	public function setProject(\MFUG\Showroom\Domain\Model\Project $project) {
+		$this->project = $project;
+	}
+
+}
+?>

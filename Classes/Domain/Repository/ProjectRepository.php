b47ce608c0b5d919a19a582e743f8b6c3a0652a4
Marco Huber <mail@marco-huber.de>
2012-02-25 12:29:13 +0100

    Create the repositories for Project and Tag
    ./flow3 kickstart:repository MFUG.Showroom Project
    ./flow3 kickstart:repository MFUG.Showroom Tag

---
+++ Classes/Domain/Repository/ProjectRepository.php
@@ -0,0 +1,21 @@
+<?php
+namespace MFUG\Showroom\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "MFUG.Showroom".              *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * A repository for Projects
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class ProjectRepository extends \TYPO3\FLOW3\Persistence\Repository {
+
+	// add customized methods here
+
+}
+?>
---
+++ Classes/Domain/Repository/TagRepository.php
@@ -0,0 +1,21 @@
+<?php
+namespace MFUG\Showroom\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "MFUG.Showroom".              *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * A repository for Tags
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class TagRepository extends \TYPO3\FLOW3\Persistence\Repository {
+
+	// add customized methods here
+
+}
+?>
